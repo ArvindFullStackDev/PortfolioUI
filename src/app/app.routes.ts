cd5fb3966e782e64f6bedef2042846c5cb824eb9
ArvindFullStackDev <arvindfullstackdev@gmail.com>
2026-07-04 18:23:04 +0530

    Change Route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,3 @@
 import { Routes } from '@angular/router';
-import { App } from './app';
 
-export const routes: Routes = [
-  // When a user visits the blank root path '', instantly redirect them to /home
-  { path: '', redirectTo: 'home', pathMatch: 'full' }, 
-  
-  // This defines the /home route and renders the AppComponent inside <router-outlet>
-  { path: 'home', component: App }
-];
+export const routes: Routes = [];
